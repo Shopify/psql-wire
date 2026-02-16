f40cd5694423aa46bb196e8a28fcc1fdc021c1e8
Jelte Fennema-Nio <github-tech@jeltef.nl>
2026-02-16 14:29:24 +0100

    Implement cache cleanup behaviour
    This cleans up statement and portal caches when receiving a Close
    message, and cleans up the portal cache when a statement in the cache
    gets overwritten.
    
    (cherry picked from commit 631005ddadd80542df00b297a6e4ed7778d149e5)

--- command.go
+++ command.go
@@ -371,6 +371,18 @@ func (srv *Session) handleParse(ctx context.Context, reader *buffer.Reader, writ
 		// `reader.GetUint32()`
 	}
 
+	existing, err := srv.Statements.Get(ctx, name)
+	if err != nil {
+		if srv.ParallelPipeline.Enabled {
+			return srv.drainQueueAndWriteError(ctx, writer, err)
+		}
+		return srv.WriteError(writer, err)
+	}
+
+	if existing != nil {
+		srv.Portals.DeleteByStatement(ctx, existing) //nolint:errcheck
+	}
+
 	if srv.ParallelPipeline.Enabled {
 		return srv.parsePipelined(ctx, writer, name, query)
 	}
@@ -714,7 +726,18 @@ func (srv *Session) handleClose(ctx context.Context, reader *buffer.Reader, writ
 		return err
 	}
 
-	srv.logger.Debug("incoming close request", slog.String("type", string(d[0])), slog.String("name", name))
+	srv.logger.Debug("incoming close request", slog.String("type", types.CloseMessage(d[0]).String()), slog.String("name", name))
+
+	switch types.CloseMessage(d[0]) {
+	case types.CloseStatement:
+		stmt, _ := srv.Statements.Get(ctx, name)
+		srv.Statements.Delete(ctx, name) //nolint:errcheck
+		if stmt != nil {
+			srv.Portals.DeleteByStatement(ctx, stmt) //nolint:errcheck
+		}
+	case types.ClosePortal:
+		srv.Portals.Delete(ctx, name) //nolint:errcheck
+	}
 
 	if srv.ParallelPipeline.Enabled {
 		srv.ResponseQueue.Enqueue(NewCloseCompleteEvent())
